db30fff9d68070203b525bbe365174211176fbd6
eswaribala <Parameswaribala@gmail.com>
2021-08-25 12:13:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',function(){
+    console.log("it's my preferred script");
+    var data=1
+    console.log(data<<2);
+    var name=prompt("Enter your name");
+    if(name!=null)
+        console.log("My Name is"+name)
+    else
+        console.log("Name missing!!!")
+})
